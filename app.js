ec4837169e7f964c1523d443377c8cce345b5579
tuskerette <vgraffeo@everquote.com>
2018-10-30 21:43:39 -0400

    add oscillator. issues with start after stop.

--- app.js
+++ app.js
@@ -1,19 +1,84 @@
-// var rangeSlider = document.querySelector('input');
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var myNoise = audioCtx.createMyNoise();
-var myGain = audioCtx.createGain();
-myGain.gain.value = 0;
-myNoise.connect(myGain);
-myGain.connect(audioCtx.destination);
 
-var startSound = function() {
-	myGain.gain.value = 1;
+// UI buttons selectors
+var start = document.querySelector('.start');
+var stop = document.querySelector('.stop');
+// var rangeSlider = document.querySelector('input');
+
+
+// Rain
+var rain = audioCtx.createRain();
+var rainGain = audioCtx.createGain();
+rainGain.gain.value = 0;
+rain.connect(rainGain);
+rainGain.connect(audioCtx.destination);
+
+// Oscillator tone
+var oscillator = audioCtx.createOscillator();
+var oscillatorGainNode = audioCtx.createGain();
+oscillator.connect(oscillatorGainNode);
+oscillatorGainNode.connect(audioCtx.destination);
+oscillator.type = 'sine';
+oscillator.frequency.setValueAtTime(25, audioCtx.currentTime); // value in hertz
+
+
+start.onclick = function() {
+	oscillator.start(audioCtx.currentTime);
+	console.log('started');
+	oscillatorGainNode.gain.value = 1;
+	// oscillator.connect(audioCtx.destination);
+	// rainGain.gain.value = 1;
 }
 
-var stopSound = function() {
-	myGain.gain.value = 0;
+stop.onclick = function() {
+	console.log('stopped');
+	oscillator.stop(audioCtx.currentTime);
+	oscillatorGainNode.gain.value = 0;
+	// oscillatorGainNode.disconnect(audioCtx.destination);
+	// oscillator.disconnect(audioCtx.destination);
+	// rainGain.gain.value = 0;
 }
+
 
 // rangeSlider.oninput = function() {
-
 // }
+
+
+
+// Stereo Panner stuff WIP
+// var panNode = audioCtx.createStereoPanner();
+// var panControl = document.querySelector('.panning-control');
+// var panValue = document.querySelector('.panning-value');
+// var pre = document.querySelector('pre');
+// var source = oscillator;
+
+// pre.innerHTML = document.querySelector('script').innerHTML;
+
+// panControl.oninput = function() {
+//   panNode.pan.setValueAtTime(panControl.value, audioCtx.currentTime);
+//   panValue.innerHTML = panControl.value;
+// }
+
+// source.connect(panNode);
+// panNode.connect(audioCtx.destination);
+
+
+
+
+
+// mute button
+// var mute = document.querySelector('.mute');
+
+// mute.onclick = function() {
+//   if(mute.getAttribute('data-muted') === 'false') {
+//     oscillatorGainNode.disconnect(audioCtx.destination);
+//     mute.setAttribute('data-muted', 'true');
+//     mute.innerHTML = "Unmute";
+//   } else {
+//     oscillatorGainNode.connect(audioCtx.destination);
+//     mute.setAttribute('data-muted', 'false');
+//     mute.innerHTML = "Mute";
+//   };
+// }
+
+
--- noise.js
+++ noise.js
@@ -1,5 +1,5 @@
 (function(AudioContext) {
-  AudioContext.prototype.createMyNoise = function(bufferSize) {
+  AudioContext.prototype.createRain = function(bufferSize) {
     bufferSize = bufferSize || 4096;
     var lastOut = 0.0;
     var node = this.createScriptProcessor(bufferSize, 1, 1);
@@ -9,9 +9,11 @@
         var white = Math.random() * 2 - 1;
         output[i] = (lastOut + (0.02 * white)) / 1.02;
         lastOut = output[i];
-        output[i] *= 2;
+        output[i] *= 0.8;
       }
     }
     return node;
   };
+
+
 })(window.AudioContext || window.webkitAudioContext);
