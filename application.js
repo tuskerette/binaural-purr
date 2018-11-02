de310d5d0097b9e46eb56a2abeda8d3ac8a37482
tuskerette <vgraffeo@everquote.com>
2018-11-02 11:24:13 -0400

    Add volumne slider

--- application.js
+++ application.js
@@ -9,6 +9,7 @@
   var analyserRight = context.createAnalyser();
   var start = document.querySelector('.start');
   var stop = document.querySelector('.stop');
+  var rangeSlider = document.querySelector('input');
 
   bBeat.connect(gain);
   gain.connect(context.destination);
@@ -19,11 +20,18 @@
   bBeat.setPitch(40);
   bBeat.setBeatRate(5);
 
+
   start.onclick = function() {
     bBeat.start();
   }
 
   stop.onclick = function() {
     bBeat.stop();
+  }
+
+  rangeSlider.oninput = function(e) {
+    var volume;
+    volume = Number(e.target.value);
+    return gain.gain.value = volume / 100;
   }
 })();
